40ed8d767fe0d113db6c96bc1d2a2da31a0a0a42
helidacosta <helidalu.oliveira@gmail.com>
2018-06-29 10:55:40 -0300

    Update Utils.java

--- src/main/java/com/tcc/helidacosta/support/Utils.java
+++ src/main/java/com/tcc/helidacosta/support/Utils.java
@@ -26,25 +26,22 @@ public class Utils {
 	 * @param propriedade nome da propriedade
 	 * @return valor da propriedade
 	 */
-	public static String lerPropriedade(String propriedade) {
-		Properties prop;
-		String valor = null;
-		try {
-			prop = new Properties();
-			prop.load(new FileInputStream(new File("config.properties")));
+public static String lerPropriedade(String propriedade) {
+	Properties prop;
+	String valor = null;
+	try {
+		prop = new Properties();
+		prop.load(new FileInputStream(new File("config.properties")));
 
-            valor = prop.getProperty(propriedade);
-			
-			if (valor == null || valor == "") {
-				throw new PropertyException();
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
+	        valor = prop.getProperty(propriedade);
+	
+		if (valor == null || valor == "") {
+			throw new PropertyException();
 		}
-		
-		return valor;
-	}
+			
+	} catch (Exception e) {	e.printStackTrace();}
+	return valor;
+}
 
     /**
      * Retorna o driver (iOS ou Android) para a execução dos testes de acordo com a plataforma informada
@@ -52,63 +49,60 @@ public class Utils {
      * @return IOSDriver para execução em iOS ou AndroidDriver para execução em Android
      * @throws MalformedURLException
      */
-	public static AppiumDriver<?> retornaDriver(String plataforma) throws MalformedURLException, 
-	PlatformException {
-		AppiumDriver<?> driver = null;
-		DesiredCapabilities capacidade = new DesiredCapabilities();
+public static AppiumDriver<?> retornaDriver(String plataforma) throws MalformedURLException, 
+PlatformException {
+	AppiumDriver<?> driver = null;
+	DesiredCapabilities capacidade = new DesiredCapabilities();
 
-		final Boolean isHybrid = Boolean.parseBoolean(lerPropriedade("execucao.hibrida"));
+	final Boolean isHybrid = Boolean.parseBoolean(lerPropriedade("execucao.hibrida"));
 		
+	if (isHybrid) {
+		capacidade.setCapability(MobileCapabilityType.AUTO_WEBVIEW, true);
+	}
+		
+	String urlCompleta = "http://" + lerPropriedade("execucao.ip") + ":" + 
+		lerPropriedade("execucao.porta") + "/wd/hub";
+		
+switch (plataforma.toLowerCase()) {
+		
+	case "ios":
+			
+	if (isHybrid) {
+		capacidade.setCapability(MobileCapabilityType.APP, 
+		new File(lerPropriedade("ios.app.hibrido")).getAbsolutePath());
+		}else {	capacidade.setCapability(MobileCapabilityType.APP, 
+			new File(lerPropriedade("ios.app.nativo")).getAbsolutePath());}
+			
+		capacidade.setCapability(MobileCapabilityType.DEVICE_NAME, 
+				 lerPropriedade("ios.nome.dispositivo"));
+		capacidade.setCapability(MobileCapabilityType.PLATFORM_VERSION, 
+					 lerPropriedade("android.versao.plataforma"));
+		capacidade.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.IOS);
+			
+		if ( Boolean.parseBoolean(lerPropriedade("ios.xcode8"))) {
+			capacidade.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
+		}
+			
+		driver = new IOSDriver<>(new URL(urlCompleta), capacidade);
+		break;
+
+	case "android":
+			
 		if (isHybrid) {
-			capacidade.setCapability(MobileCapabilityType.AUTO_WEBVIEW, true);
-		}
-		
-		String urlCompleta = "http://" + lerPropriedade("execucao.ip") + ":" + 
-			lerPropriedade("execucao.porta") + "/wd/hub";
-		
-		switch (plataforma.toLowerCase()) {
-		
-		case "ios":
+			capacidade.setCapability(MobileCapabilityType.APP, 
+			new File(lerPropriedade("android.app.hibrido")).getAbsolutePath());}
+		else {capacidade.setCapability(MobileCapabilityType.APP, 
+			new File(lerPropriedade("android.app.nativo")).getAbsolutePath());}
 			
-			if (isHybrid) {
-				capacidade.setCapability(MobileCapabilityType.APP, 
-				new File(lerPropriedade("ios.app.hibrido")).getAbsolutePath());
-			}else {	capacidade.setCapability(MobileCapabilityType.APP, 
-				new File(lerPropriedade("ios.app.nativo")).getAbsolutePath());}
+		capacidade.setCapability(MobileCapabilityType.DEVICE_NAME, 
+					 lerPropriedade("android.nome.dispositivo"));
+		capacidade.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
 			
-			capacidade.setCapability(MobileCapabilityType.DEVICE_NAME, 
-						 lerPropriedade("ios.nome.dispositivo"));
-			capacidade.setCapability(MobileCapabilityType.PLATFORM_VERSION, 
-						 lerPropriedade("android.versao.plataforma"));
-			capacidade.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.IOS);
+		driver = new AndroidDriver<>(new URL(urlCompleta), capacidade);
+		break;
 			
-			if ( Boolean.parseBoolean(lerPropriedade("ios.xcode8"))) {
-				capacidade.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
-			}
-			
-			driver = new IOSDriver<>(new URL(urlCompleta), capacidade);
-			break;
-
-		case "android":
-			
-			if (isHybrid) {
-				capacidade.setCapability(MobileCapabilityType.APP, 
-				new File(lerPropriedade("android.app.hibrido")).getAbsolutePath());}
-			else {capacidade.setCapability(MobileCapabilityType.APP, 
-				new File(lerPropriedade("android.app.nativo")).getAbsolutePath());}
-			
-			capacidade.setCapability(MobileCapabilityType.DEVICE_NAME, 
-						 lerPropriedade("android.nome.dispositivo"));
-			capacidade.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
-			
-			driver = new AndroidDriver<>(new URL(urlCompleta), capacidade);
-			break;
-			
-		    default:
-		        throw new PlatformException();
-
-		}
-		
-		return driver;
+	default: throw new PlatformException();
+	}
+	return driver;
 	}
 }
